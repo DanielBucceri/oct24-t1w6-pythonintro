1f1e1780a07ebe08759a5f32b59f36764f6b1a5c
Daniel <gcas012125@coderacademy.edu.au>
2024-11-20 20:44:42 +1100

    Match case example

--- main.py
+++ main.py
@@ -7,25 +7,34 @@
 # distance = int(input("Enter the distance the rocket will travel: "))
 # calculate_rocket_fuel_required(distance)
 
-def calculate_ticket_price_for_performer(regular_ticket_price, performer_type):
-   if performer_type == "Juggler":
-        cost = regular_ticket_price - (regular_ticket_price * 0.5)
-        print(cost)
-   elif  performer_type == "Fire twirlers":
-        cost = regular_ticket_price - (regular_ticket_price * 0.25)
-        print(cost)
-   elif performer_type == "Magician":
-        cost = regular_ticket_price - (regular_ticket_price * 0.80)
-        print(cost)
-   elif performer_type == "Escape Artist":
-        cost = regular_ticket_price - (regular_ticket_price * 1)
-        print(cost)
-   else:
-        cost = regular_ticket_price
-        print(cost)
+# def calculate_ticket_price_for_performer(regular_ticket_price, performer_type):
+#    if performer_type == "Juggler":
+#         cost = regular_ticket_price - (regular_ticket_price * 0.5)
+#         print(cost)
+#    elif  performer_type == "Fire twirlers":
+#         cost = regular_ticket_price - (regular_ticket_price * 0.25)
+#         print(cost)
+#    elif performer_type == "Magician":
+#         cost = regular_ticket_price - (regular_ticket_price * 0.80)
+#         print(cost)
+#    elif performer_type == "Escape Artist":
+#         cost = regular_ticket_price - (regular_ticket_price * 1)
+#         print(cost)
+#    else:
+#         cost = regular_ticket_price
+#         print(cost)
 
-   return cost
+#    return cost
 
-price = int(input("enter ticket price: "))
-performer = input("Enter performer type: ")
-calculate_ticket_price_for_performer(price, performer)
+# price = int(input("enter ticket price: "))
+# performer = input("Enter performer type: ")
+# calculate_ticket_price_for_performer(price, performer)
+
+day_of_week = 7
+match day_of_week:
+     case 1 | 2 | 3 | 4 | 5:
+          print("Tis a weekday")
+     case 6 | 7:
+          print("Tis weekEND")
+     case 6 | 7:
+        print("Tis weekEND")
